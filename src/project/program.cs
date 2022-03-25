e32cd77c9ad6881c9b3f64725e64692a24fca0f6
Maurice de Jong <maurieezzz@hotmail.com>
2022-03-25 12:16:56 +0100

    textual imperfection

--- src/project/program.cs
+++ src/project/program.cs
@@ -2,4 +2,4 @@ using System.Reflection;
 using System;
 // if you run this program it will output it's informational version attribute.
 var version = Assembly.GetEntryAssembly()!.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion ?? "empty or not set during build";
-Console.WriteLine($"Hello, World! thi is version: {version}");
+Console.WriteLine($"Hello, World! this is version: {version}");
