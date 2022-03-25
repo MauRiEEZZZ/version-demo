5c595ce8e4de77b37ac793623193f5654bae1ce0
Maurice de Jong <maurieezzz@hotmail.com>
2022-03-25 12:12:10 +0100

    using corrected

--- src/project/program.cs
+++ src/project/program.cs
@@ -1,4 +1,5 @@
 using System.Reflection;
+using System;
 // if you run this program it will output it's informational version attribute.
 var version = Assembly.GetEntryAssembly()!.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion ?? "empty or not set during build";
 Console.WriteLine($"Hello, World! thi is version: {version}");
